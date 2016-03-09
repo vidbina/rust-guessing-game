97b0581a5db5d8be2ef3bb660bab09893d45872c
David Asabina <david@supr.nu>
2016-03-09 20:55:43 +0100

    Keep score and break on success :dart:

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     println!("Guess away!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut score = 0;
 
     loop {
         println!("Input your guess *drumroll*!");
@@ -19,10 +20,15 @@ fn main() {
 
         let guess: u32 = guess.trim().parse().expect("Has to be a number, buddy!");
 
+        score += 1;
+
         match guess.cmp(&secret_number) {
             Ordering::Less    => println!("Not quite"),
             Ordering::Greater => println!("Whoah there, cowboy!"),
-            Ordering::Equal   => println!("Spot on!"),
+            Ordering::Equal   => {
+                println!("Spot on in {} steps!", score);
+                break;
+            }
         }
     }
 }
