6a97e3081450a14e98f3a5793e10ed99612db893
David Asabina <david@supr.nu>
2016-03-09 20:33:00 +0100

    Generate random number

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess away!");
-    println!("Input your guess");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("Input your guess. Pssst, it should be {}", secret_number);
 
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess).expect("Failed to read line");
-    println!("You guessed {}", guess)
+    println!("You guessed {}", guess);
 }
